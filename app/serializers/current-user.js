0fe8d1fa7dd0c2d478f2688b6aad635de902009c
begedin <begedinnikola@gmail.com>
2015-04-22 17:58:24 +0200

    Simplified serializer

--- app/serializers/current-user.js
+++ app/serializers/current-user.js
@@ -1,11 +1,7 @@
 import ApplicationSerializer from 'teamplaybook-ember/serializers/application';
 
 export default ApplicationSerializer.extend({
-  typeForRoot: function (prop) {
-    if (prop === 'users') {
-      return 'currentUser';
-    } else {
-      return this._super(prop);
-    }
+  typeForRoot: function () {
+    return 'currentUser';
   }
 });
