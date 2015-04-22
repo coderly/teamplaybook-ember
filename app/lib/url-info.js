709740f28bda9d72f1fb803e6dbe65e66d7ef0f3
begedin <begedinnikola@gmail.com>
2015-04-22 09:27:39 +0200

    Refactored url-info library to make it less confusing

--- app/lib/url-info.js
+++ app/lib/url-info.js
@@ -3,27 +3,41 @@ import ENV from '../config/environment';
 
 var urlInfo = Ember.Object.extend({
 
-  subdomain: function(){
-    var regexParse = new RegExp('[a-z-0-9]{2,63}.[a-z.]{2,5}$');
-    var urlParts = regexParse.exec(this.get('hostname'));
-    if(urlParts) {
-      return this.normalize(this.get('hostname').replace(urlParts[0],'').slice(0, -1));
-    } else {
-      return this.normalize('');
-    }
-  }.property('hostname'),
-
-  isOnRegularSubdomain: Ember.computed.equal('subdomain', 'default'),
-  isOnOrganizationSubdomain: Ember.computed.not('isOnRegularSubdomain'),
 
   hostname: function(){
     return window.location.hostname;
   }.property(),
 
-  normalize: function (subdomain) {
-    return ENV.subdomainMapping[subdomain] || subdomain;
+  subdomain: function(){
+    var hostname = this.get('hostname'),
+        baseUrl = this._extractBaseUrl(hostname);
+
+    return this._extractSubdomain(hostname, baseUrl);
+  }.property('hostname'),
+
+  isOnRegularSubdomain: Ember.computed.equal('subdomain', 'default'),
+  isOnOrganizationSubdomain: Ember.computed.not('isOnRegularSubdomain'),
+
+  _extractBaseUrl: function (hostname) {
+    var lastTwoPiecesOfHostnameRegex = new RegExp('[a-z-0-9]{2,63}.[a-z.]{2,5}$');
+    return lastTwoPiecesOfHostnameRegex.exec(hostname);
   },
 
+  _extractSubdomain: function (hostname, baseUrl) {
+
+    var subdomain = '';
+
+    if(baseUrl) {
+      var subdomainWithDot = this._applySubdomainMapping(hostname.replace(baseUrl,''));
+      subdomain = subdomainWithDot.slice(0, -1);
+    }
+
+    return subdomain;
+  },
+
+  _applySubdomainMapping: function (subdomain) {
+    return ENV.subdomainMapping[subdomain] || subdomain;
+  },
 });
 
 export default urlInfo;
