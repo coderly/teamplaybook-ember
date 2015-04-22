99fe0e2dce9a3b761b9be9ed69df5e03d478232c
begedin <begedinnikola@gmail.com>
2015-04-22 16:29:18 +0200

    Simplified existing adapters

--- app/adapters/current-user.js
+++ app/adapters/current-user.js
@@ -1,7 +1,7 @@
 import ApplicationAdapter from 'teamplaybook-ember/adapters/application';
 
 export default ApplicationAdapter.extend({
-  find: function () {
-    return this.ajax(this.fullUrl('me'), 'GET');
+  buildURL: function () {
+    return this.get('host') + '/me';
   }
 });
