0c4427853356d594a328db30cbc0f2e6d36e96b8
begedin <begedinnikola@gmail.com>
2015-04-24 14:09:24 +0200

    Added relationships for user and team models, removed fixtures

--- app/models/user.js
+++ app/models/user.js
@@ -2,15 +2,8 @@ import DS from 'ember-data';
 
 var User = DS.Model.extend({
   username: DS.attr('string'),
-  teams: DS.hasMany('teams', { async: true })
-});
-
-User.reopenClass({
-  FIXTURES: [
-    { id: 1, username: 'johndoe', teams: [1] },
-    { id: 2, username: 'janedoe', teams: [1, 2] },
-    { id: 3, username: 'johnsmith', teams: [2] }
-  ]
+  teams: DS.hasMany('teams', { async: true }),
+  teamMemberships: DS.hasMany('team-membership', { async: true })
 });
 
 export default User;
