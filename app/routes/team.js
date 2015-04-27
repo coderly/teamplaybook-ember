f5d4ea18e113c5d277e1fcf9fd61d58f3a79781a
begedin <begedinnikola@gmail.com>
2015-04-27 17:03:55 +0200

    Refactored team adapter to make it a bit clearer

--- app/routes/team.js
+++ app/routes/team.js
@@ -3,6 +3,6 @@ import AuthenticatedRouteMixin from 'simple-auth/mixins/authenticated-route-mixi
 
 export default Ember.Route.extend(AuthenticatedRouteMixin, {
   model: function () {
-    return this.store.find('team', this.get('urlInfo.subdomain'));
+    return this.store.find('team', 'current');
   }
 });
