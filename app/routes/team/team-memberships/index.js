934bae8ecc07b337790f0d33b73a3742f96bfcd9
begedin <begedinnikola@gmail.com>
2015-04-28 16:52:46 +0200

    Added listing of team memberships

--- app/routes/team/team-memberships/index.js
+++ app/routes/team/team-memberships/index.js
@@ -1,6 +1,10 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  model: function () {
+    return this.store.find('team-membership');
+  },
+
   resetController: function(controller) {
     return controller.setProperties({
       showError: false,
