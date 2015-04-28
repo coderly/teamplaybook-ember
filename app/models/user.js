d976527a03d9d10df248b83a9b0e4b12205abf64
begedin <begedinnikola@gmail.com>
2015-04-28 16:36:53 +0200

    Initial functionality. User can view a list of team members.

--- app/models/user.js
+++ app/models/user.js
@@ -1,7 +1,7 @@
 import DS from 'ember-data';
 
 export default DS.Model.extend({
-  username: DS.attr('string'),
-  teams: DS.hasMany('teams', { async: true }),
+  email: DS.attr('string'),
+  teams: DS.hasMany('teams', { async: true, inverse: 'members' }),
   teamMemberships: DS.hasMany('team-membership', { async: true })
 });
