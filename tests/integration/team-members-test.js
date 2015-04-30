9c9f77bb42a159c8d5e39b09e8f2a1148ff70d82
begedin <begedinnikola@gmail.com>
2015-04-30 13:10:59 +0200

    Finalized spec for team members list

--- tests/integration/team-members-test.js
+++ tests/integration/team-members-test.js
@@ -21,13 +21,13 @@ module('Team members', {
     server = mockServer(function() {
       this.post('accounts/tokens', response(200, loginSuccessResponse));
       this.get('team', response(200, basicTeamResponse));
-      //this.get('users', response(200, listOfNTeamMembersResponseBuilder(0)));
+      this.get('users', response(200, listOfNTeamMembersResponseBuilder(1)));
     });
     App = startApp({ subdomain: 'test'});
   },
 
   afterEach: function() {
-    // Todo - Remove this when we update Ember from 1.11.1
+    // Todo - Remove this when we update Ember from< 1.11.1
     // see https://github.com/emberjs/ember.js/issues/10310#issuecomment-95685137
     App.registry = App.buildRegistry();
     App.reset();
@@ -50,6 +50,23 @@ test('Navigating to "/members"', function(assert) {
   visit('members');
 
   andThen(function() {
-    assert.equal(currentRouteName(), 'team.team-members.index', 'Actually navigates to "team.team-members.index"');
+    assert.equal(currentRouteName(), 'team.team-members.index', 'Actually navigates to "team.team-members.index".');
+  });
+});
+
+test('Team members list', function(assert) {
+  assert.expect(2);
+
+  server.get('users', function() {
+    assert.ok(true, 'Fetches list of members from the API.');
+    return buildResponse(200, listOfNTeamMembersResponseBuilder(5));
+  });
+
+  visit('login');
+  login();
+  visit('members');
+
+  andThen(function() {
+    assert.equal(find('tbody tr').length, 5, 'Shows an entry for each user.');
   });
 });
--- tests/mocks/user.js
+++ tests/mocks/user.js
@@ -1,7 +1,7 @@
 function listOfNTeamMembersResponseBuilder(memberCount) {
   var data = [];
   for (let i = 0; i < memberCount; i++) {
-    var teamMember = { id: i, email: `member${i}@test.com` };
+    var teamMember = { id: i, email: `member${i}@test.com`, type: 'users' };
     data.push(teamMember);
   }
 
