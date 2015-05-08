b47279480c25102343cf196c5ba0311ba7f9bddd
israel de la hoz <israeldelahoz@gmail.com>
2015-05-08 00:50:03 -0500

    Add stripe

--- app/controllers/team/settings.js
+++ app/controllers/team/settings.js
@@ -1,7 +1,55 @@
 import Ember from 'ember';
+import UrlInfo from 'teamplaybook-ember/lib/url-info';
+import ajax from 'ic-ajax';
 
 export default Ember.Controller.extend({
+  cardToken: null,
+
   plans: Ember.computed(function(){
     return this.store.find('plan');
-  })
+  }),
+
+  actions:{
+    changePlan: function(){
+      var plans = this.store.all('plan');
+      var plan = plans.findBy('slug', this.get('model.planSlug'));
+
+      if(plan.isPaidPlan){
+        createStripeToken(requestPlanChange);
+      }else{
+        requestPlanChange();
+      }
+    }
+  },
+
+  _buildURL: function(path) {
+    var apiUrl = this.get('urlInfo.apiUrl');
+    return apiUrl + '/' + path;
+  },
+
+  requestPlanChange: function(){
+    var controller = this;
+
+    ajax({
+     type: 'POST',
+     url: this._buildURL('team/change_plan'),
+     data: {
+       plan_slug: this.get('model.planSlug'),
+       card_token: this.get('cardToken')
+     }
+    }).then(function(){
+      controller.get('model').reload();
+    });
+  },
+
+  createStripeToken: function(callback){
+    var controller = this;
+    var $form = $('#payment-form');
+
+    stripeCallback = function(status, response){
+      controller.set('cardToken', response.id);
+      callback();
+    }
+    Stripe.card.createToken($form, stripeCallback);
+  }
 });
--- app/models/plan.js
+++ app/models/plan.js
@@ -3,5 +3,7 @@ import DS from 'ember-data';
 export default DS.Model.extend({
   slug: DS.attr('string'),
   name: DS.attr('string'),
-  trial_period_days: DS.attr('number')
+  trial_period_days: DS.attr('number'),
+  amount: DS.attr('number'),
+  isPaidPlan: Ember.computed.gt('amount', 0)
 });
