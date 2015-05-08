f82d4eeddb4206d369fded790f493b73294c6185
begedin <begedinnikola@gmail.com>
2015-05-08 10:05:31 +0200

    Interface improvements for team-options-menu

--- app/components/team-options-menu.js
+++ app/components/team-options-menu.js
@@ -4,11 +4,31 @@ import ClickOutsideMixin from 'teamplaybook-ember/mixins/click-outside';
 export default Ember.Component.extend(ClickOutsideMixin, {
   classNames: ['team-options-menu'],
 
+  team: null,
+  currentUser: null,
+
+  currentUsersMembershipInCurrentTeam: function() {
+
+  }.property('team', 'currentUser'),
+
+  currentUsersRoleInCurrentTeam: Ember.computed.alias('currentUsersMemberShipInCurrentTeam.role'),
+  isCurrentUserMember: Ember.computed.equal('currentUsersRoleInCurrentTeam', 'member'),
+  isCurrentUserAdmin: Ember.computed.equal('currentUsersRoleInCurrentTeam', 'admin'),
+  isCurrentUserAllowedToLeaveTeam: Ember.computed.or('isCurrentUserAdmin', 'isCurrentUserMember'),
+
   isMenuVisible: false,
 
   actions: {
     showMenu: function() {
       this.set('isMenuVisible', true);
+    },
+
+    logout: function() {
+      this.sendAction('logout');
+    },
+
+    leaveTeam: function() {
+      this.sendAction('leaveTeam');
     }
   },
 
