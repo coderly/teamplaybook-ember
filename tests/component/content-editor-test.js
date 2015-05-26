248498cc5eb1feef409249708ccb788484d8d666
Nikola Begedin <begedinnikola@gmail.com>
2015-05-26 09:17:02 +0200

    Toolbar visibility test

--- tests/component/content-editor-test.js
+++ tests/component/content-editor-test.js
@@ -10,3 +10,17 @@ test('The html content of the ".content" element is set to value', function(asse
   });
   assert.equal(this.$().find('.content').html(), '<b>Lorem</b> ipsum');
 });
+
+test('The ".toolbar" rendering is determined by "disableToolbar" flag', function(assert) {
+  var component = this.subject({
+    disableToolbar: false
+  });
+
+  assert.equal(this.$().find('.toolbar').length, 1, 'toolbar is rendered');
+
+  Ember.run(function() {
+    component.set('disableToolbar', true);
+  });
+
+  assert.equal(this.$().find('.toolbar').length, 0, 'toolbar is not rendered');
+});
