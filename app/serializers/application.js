25c0ec500165bd43a14dc002f8eca2a80c7afc03
israel de la hoz <israeldelahoz@gmail.com>
2015-05-26 18:14:23 -0500

    fix problem with attributes camelize

--- app/serializers/application.js
+++ app/serializers/application.js
@@ -1,3 +1,7 @@
 import JsonApiSerializer from 'ember-json-api/json-api-serializer';
 
-export default JsonApiSerializer.extend({});
+export default JsonApiSerializer.extend({
+  keyForAttribute: function(key) {
+    return Ember.String.decamelize(key);
+  }
+});
