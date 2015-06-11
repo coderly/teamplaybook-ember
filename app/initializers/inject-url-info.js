fcfa1eafe0c03715f1cfb4d319e34873f1b70294
begedin <begedinnikola@gmail.com>
2015-06-11 14:06:46 +0200

    Fixed some deprecations

--- app/initializers/inject-url-info.js
+++ app/initializers/inject-url-info.js
@@ -2,8 +2,8 @@ import UrlInfo from 'teamplaybook-ember/lib/url-info';
 
 export default {
   name: 'inject-url-info',
-  initialize: function(container, application) {
-    container.register('url-info:main', UrlInfo);
+  initialize: function(registry, application) {
+    application.register('url-info:main', UrlInfo);
     application.inject('adapter', 'urlInfo', 'url-info:main');
     application.inject('route', 'urlInfo', 'url-info:main');
     application.inject('controller', 'urlInfo', 'url-info:main');
