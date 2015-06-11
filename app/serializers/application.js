c491f9ecea6d0d8eb1d45f0086f5944a646ae4c2
begedin <begedinnikola@gmail.com>
2015-06-11 15:18:14 +0200

    Monkeypathc for issue with serializeHasMany in ember-json-api

--- app/serializers/application.js
+++ app/serializers/application.js
@@ -1,3 +1,31 @@
+import Ember from 'ember';
 import JsonApiSerializer from 'ember-json-api/json-api-serializer';
 
-export default JsonApiSerializer.extend({});
+function hasManyLink(key, type, record, attr) {
+  var links = Ember.A(record.hasMany(attr)).mapBy('id') || [];
+  var typeName = Ember.String.pluralize(type);
+  var linkages = [];
+  var index, total;
+
+  for(index=0, total=links.length; index<total; ++index) {
+    linkages.push({
+      id: links[index],
+      type: typeName
+    });
+  }
+
+  return { linkage: linkages };
+}
+
+export default JsonApiSerializer.extend({
+  serializeHasMany: function(record, json, relationship) {
+    var attr = relationship.key;
+    var type = this.keyForRelationship(relationship.type);
+    var key = this.keyForRelationship(attr);
+
+    if (relationship.kind === 'hasMany') {
+      json.links = json.links || {};
+      json.links[key] = hasManyLink(key, type, record, attr);
+    }
+  }
+});
